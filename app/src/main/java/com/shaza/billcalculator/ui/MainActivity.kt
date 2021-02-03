77b05a600e80513b5d4670407be24f6cbec3ea09
Shaza Hassan <Shaza.hassan@coformatique.com>
2021-02-03 09:22:07 +0200

    show details for selected bill

--- app/src/main/java/com/shaza/billcalculator/ui/MainActivity.kt
+++ app/src/main/java/com/shaza/billcalculator/ui/MainActivity.kt
@@ -2,11 +2,26 @@ package com.shaza.billcalculator.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.shaza.billcalculator.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController, null)
+//        NavigationUI.setupWithNavController(null, navController)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        return NavigationUI.navigateUp(navController, null)
+    }
+
 }
