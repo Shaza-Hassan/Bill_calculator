6b78b3a535b1ff979c9d2ce2cec73fe1035bc17e
Shaza Hassan <Shaza.hassan@coformatique.com>
2021-02-08 09:49:27 +0200

    Handle navigation

--- app/src/main/java/com/shaza/billcalculator/ui/MainActivity.kt
+++ app/src/main/java/com/shaza/billcalculator/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.shaza.billcalculator.ui
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -14,8 +15,18 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         NavigationUI.setupActionBarWithNavController(this, navController, null)
-//        NavigationUI.setupWithNavController(null, navController)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home ->{
+                onBackPressed()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
